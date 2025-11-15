4255b69255d0e188d5caab6fca29340a729020e8
mkultra69 <mkultra69@otso.city>
2025-11-15 18:10:39 +0500

    fix: исправлен base path в vite.config для GitHub Pages

--- efans/vite.config.ts
+++ efans/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/efans/',
+  base: '/qdok/efans/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
